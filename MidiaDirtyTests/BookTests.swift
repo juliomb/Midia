8ad9feb5ae7913cadb0416a839e316c8f60a9825
Julio Martinez <juliomb89@gmail.com>
2019-02-15 12:21:28 +0100

    Added target membership for book model. Created book existence test

--- MidiaDirtyTests/BookTests.swift
+++ MidiaDirtyTests/BookTests.swift
@@ -10,24 +10,14 @@ import XCTest
 
 class BookTests: XCTestCase {
 
+    var bestBookEver: Book!
+
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        bestBookEver = Book(bookId: "1", title: "El nombre del viento", authors: ["Patrick Rothfuss"], publishedDate: Date(timeIntervalSinceNow: 13213), description: "Kvothe rules", coverURL: nil, rating: 5, numberOfReviews: 2131, price: 10)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testBookExistence() {
+        XCTAssertNotNil(bestBookEver)
     }
 
 }
