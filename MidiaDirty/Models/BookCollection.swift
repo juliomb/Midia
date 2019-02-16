f12e8051391703b296ced1e2ed21d0c27c955d2d
Julio Martinez <juliomb89@gmail.com>
2019-02-16 10:59:54 +0100

    Created book collection model

---
+++ MidiaDirty/Models/BookCollection.swift
@@ -0,0 +1,17 @@
+//
+//  BookCollection.swift
+//  MidiaDirty
+//
+//  Created by Julio Martínez Ballester on 2/16/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import Foundation
+
+struct BookCollection {
+
+    let kind: String
+    let totalItems: Int
+    let items: [Book]?
+
+}
