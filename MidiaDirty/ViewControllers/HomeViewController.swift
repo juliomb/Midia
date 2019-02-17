402e592c068e36bab603731c7aefc15ebbae8f4f
Julio Martinez <juliomb89@gmail.com>
2019-02-17 11:07:34 +0100

    Created empty home view controller and added a collection view in the storyboard

---
+++ MidiaDirty/ViewControllers/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  MidiaDirty
+//
+//  Created by Julio Martínez Ballester on 2/17/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+}
