6c944f8ca36328df4230a43f71d7e9e854f06032
Julio Martinez <juliomb89@gmail.com>
2019-02-17 19:04:14 +0100

    Created mediaitemprovidable protocol

---
+++ MidiaDirty/Protocols/MediaItemProvidable.swift
@@ -0,0 +1,16 @@
+//
+//  MediaItemProvidable.swift
+//  MidiaDirty
+//
+//  Created by Julio Martínez Ballester on 2/17/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import Foundation
+
+protocol MediaItemProvidable {
+
+    var title: String { get }
+    var imageURL: URL? { get }
+    
+}
