180099af7cbc944f1fa37d1cdc86f1ef2e8908a1
Julio Martinez <juliomb89@gmail.com>
2019-02-20 11:18:22 +0000

    Created media item provider with an enum for the media types

---
+++ MidiaDirty/Network/MediaItemProvider.swift
@@ -0,0 +1,15 @@
+//
+//  MediaItemProvider.swift
+//  MidiaDirty
+//
+//  Created by Julio Martínez Ballester on 2/20/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import Foundation
+
+enum MediaItemKind {
+    case book
+    case movie
+    case game
+}
