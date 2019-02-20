1a55c2ab0ddf5b841f9103172664c174d69341e4
Julio Martinez <juliomb89@gmail.com>
2019-02-20 11:54:33 +0000

    Added MediaItemAPIConsumable protocol

---
+++ MidiaDirty/Network/MediaItemAPIConsumable.swift
@@ -0,0 +1,15 @@
+//
+//  MediaItemAPIConsumable.swift
+//  MidiaDirty
+//
+//  Created by Julio Martínez Ballester on 2/20/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import Foundation
+
+protocol MediaItemAPIConsumable {
+
+    func getLatestMediaItems() -> [MediaItemProvidable]
+
+}
