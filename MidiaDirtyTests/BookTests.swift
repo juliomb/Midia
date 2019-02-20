5d25d455c8a354529509ff80490a003df0cc4ebc
Julio Martinez <juliomb89@gmail.com>
2019-02-20 12:14:35 +0000

    tests fixed using book constructor and adding target membership

--- MidiaDirtyTests/BookTests.swift
+++ MidiaDirtyTests/BookTests.swift
@@ -13,7 +13,7 @@ class BookTests: XCTestCase {
     var bestBookEver: Book!
 
     override func setUp() {
-        bestBookEver = Book(bookId: "1", title: "El nombre del viento", authors: ["Patrick Rothfuss"], publishedDate: Date(timeIntervalSinceNow: 13213), description: "Kvothe rules", coverURL: nil, rating: 5, numberOfReviews: 2131, price: 10)
+        bestBookEver = Book(bookId: "1", title: "El nombre del viento")
     }
 
     func testBookExistence() {
