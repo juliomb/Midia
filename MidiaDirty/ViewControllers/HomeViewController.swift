e7f591ce32464f975f15209eadf1b75eeea181ad
Julio Martinez <juliomb89@gmail.com>
2019-02-21 11:20:14 +0000

    Implemented states logic

--- MidiaDirty/ViewControllers/HomeViewController.swift
+++ MidiaDirty/ViewControllers/HomeViewController.swift
@@ -17,12 +17,43 @@ class HomeViewController: UIViewController {
         case ready
     }
 
-    var state: HomeViewControllerState = .loading
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var stateEmojiLabel: UILabel!
+    @IBOutlet weak var stateTextLabel: UILabel!
+
+    var state: HomeViewControllerState = .loading {
+        willSet {
+            guard state != newValue else { return }
+
+            // Ocultamos todo  y después mostramos lo correcto
+            for view: UIView in [collectionView, activityIndicator, stateEmojiLabel, stateTextLabel] {
+                view.isHidden = true
+            }
+
+            switch newValue {
+            case .loading:
+                activityIndicator.isHidden = false
+            case .noResults:
+                stateEmojiLabel.isHidden = false
+                stateTextLabel.isHidden = false
+                stateEmojiLabel.text = "☹️"
+                stateTextLabel.text = "No hay nada!!!"
+            case .noConnection:
+                stateEmojiLabel.isHidden = false
+                stateTextLabel.isHidden = false
+                stateEmojiLabel.text = "❌"
+                stateTextLabel.text = "No hay conexión"
+            case .ready:
+                collectionView.isHidden = false
+                collectionView.reloadData()
+            }
+        }
+    }
 
     let mediaItemCellIdentifier = "MediaItemCell"
 
     var mediaItemProvider: MediaItemProvider!
-    var mediaItems: [MediaItemProvidable]!
+    var mediaItems: [MediaItemProvidable]! = []
 
     @IBOutlet weak var collectionView: UICollectionView!
 
@@ -32,12 +63,11 @@ class HomeViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
 
-        // TODO: vista cargando
         mediaItemProvider.getLatestMediaItems(onSuccess: { [weak self] mediaItems in
             self?.mediaItems = mediaItems
-            self?.collectionView.reloadData()
-        }) { errorMessage in
-            // TODO: mostrar mensaje de error
+            self?.state = mediaItems.count > 0 ? .ready : .noResults
+        }) { [weak self] errorMessage in
+            self?.state = .noConnection
         }
     }
 
