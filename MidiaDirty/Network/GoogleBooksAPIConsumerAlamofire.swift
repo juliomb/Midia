fc31ce3e7b993e38bfc3054cebc24a96a1656f64
Julio Martinez <juliomb89@gmail.com>
2019-02-22 11:52:36 +0000

    Installed cocoapods, added Alamofire, created Alamofire consumer

---
+++ MidiaDirty/Network/GoogleBooksAPIConsumerAlamofire.swift
@@ -0,0 +1,18 @@
+//
+//  GoogleBooksAPIConsumerAlamofire.swift
+//  MidiaDirty
+//
+//  Created by Julio Martínez Ballester on 2/22/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class GoogleBooksAPIConsumerAlamofire: MediaItemAPIConsumable {
+    
+    func getLatestMediaItems(onSuccess success: @escaping ([MediaItemProvidable]) -> Void, failure: @escaping (Error) -> Void) {
+        <#code#>
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-MidiaDirtyTests/Pods-MidiaDirtyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MidiaDirtyTests : NSObject
+@end
+@implementation PodsDummy_Pods_MidiaDirtyTests
+@end
