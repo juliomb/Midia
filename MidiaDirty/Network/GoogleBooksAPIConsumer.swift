8e1d740d3bc49b8c72fdf12c57e20d000e154d40
Julio Martinez <juliomb89@gmail.com>
2019-02-22 10:31:00 +0000

    created GoogleBooksAPIConsumer

---
+++ MidiaDirty/Network/GoogleBooksAPIConsumer.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleBooksAPIConsumer.swift
+//  MidiaDirty
+//
+//  Created by Julio Martínez Ballester on 2/22/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import Foundation
+
+class GoogleBooksAPIConsumer: MediaItemAPIConsumable {
+
+    func getLatestMediaItems(onSuccess success: @escaping ([MediaItemProvidable]) -> Void, failure: @escaping (Error) -> Void) {
+        
+    }
+
+}
