2701854a90a410ff980583312ecf4e5ebc2e0c76
Julio Martinez <juliomb89@gmail.com>
2019-02-23 22:16:24 +0000

    implemented error handling for retrieving detail

--- MidiaDirty/ViewControllers/DetailViewController.swift
+++ MidiaDirty/ViewControllers/DetailViewController.swift
@@ -24,7 +24,11 @@ class DetailViewController: UIViewController {
         mediaItemProvider.getMediaItem(byId: mediaItemId, success: { [weak self] (detailedMediaItem) in
             self?.detailedMediaItem = detailedMediaItem
         }) { [weak self] (error) in
-            // TODO: ahora mismo
+            let alertController = UIAlertController(title: nil, message: "Error recuperando los detalles del item. Disculpe las molestias.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
+                self?.dismiss(animated: true, completion: nil)
+            }))
+            self?.present(alertController, animated: true, completion: nil)
         }
     }
 
