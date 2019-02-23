36f90d6e5086dca2d236498a6b3c6e4bcba15e5d
Julio Martinez <juliomb89@gmail.com>
2019-02-23 20:55:21 +0000

    Added Detail storyboard and VC

---
+++ MidiaDirty/ViewControllers/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  MidiaDirty
+//
+//  Created by Julio Martínez Ballester on 2/23/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+}
