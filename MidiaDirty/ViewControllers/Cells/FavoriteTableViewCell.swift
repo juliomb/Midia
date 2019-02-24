d481cd292174cc4d847c51babec115d307e07f59
Julio Martinez <juliomb89@gmail.com>
2019-02-24 18:11:40 +0000

    added favorite table view cell

---
+++ MidiaDirty/ViewControllers/Cells/FavoriteTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteTableViewCell.swift
+//  MidiaDirty
+//
+//  Created by Julio Martínez Ballester on 2/24/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+
+
+}
