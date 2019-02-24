6faaf5efe47912499c13c41d0130a64aa68190bc
Julio Martinez <juliomb89@gmail.com>
2019-02-24 17:45:18 +0000

    created favoritesProvidable protocol

---
+++ MidiaDirty/Persistence/FavoritesProvidable.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesProvidable.swift
+//  MidiaDirty
+//
+//  Created by Julio Martínez Ballester on 2/24/19.
+//  Copyright © 2019 Yuju. All rights reserved.
+//
+
+import Foundation
+
+protocol FavoritesProvidable {
+
+    func getFavoritesList() -> [MediaItemProvidable]?
+    func getFavorite(withId id: String) -> MediaItemDetailProvidable
+
+}
