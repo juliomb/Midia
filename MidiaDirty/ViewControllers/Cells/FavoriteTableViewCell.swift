a8433d702bba7bb664eea3f187831d2ec2a7061e
Julio Martinez <juliomb89@gmail.com>
2019-02-24 18:27:25 +0000

    logic to populate favorite cell

--- MidiaDirty/ViewControllers/Cells/FavoriteTableViewCell.swift
+++ MidiaDirty/ViewControllers/Cells/FavoriteTableViewCell.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import SDWebImage
 
 class FavoriteTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var mediaItemImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var creatorsLabel: UILabel!
     @IBOutlet weak var creationDateLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
 
-    
+    var mediaItem: MediaItemDetailProvidable! {
+        didSet {
+            titleLabel.text = mediaItem.title
+            if let url = mediaItem.imageURL {
+                mediaItemImageView.sd_setImage(with: url, placeholderImage: UIImage(named: "placeholder.png"))
+            }
+            if let creator = mediaItem.creatorName {
+                creatorsLabel.text = creator
+            } else {
+                creatorsLabel.isHidden = true
+            }
+            if let date = mediaItem.creationDate {
+                creationDateLabel.text = DateFormatter.booksAPIDateFormater.string(from: date)
+            } else {
+                creationDateLabel.isHidden = true
+            }
+            if let price = mediaItem.price {
+                priceLabel.text = "Price \(price)"
+            } else {
+                priceLabel.isHidden = true
+            }
+        }
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        mediaItemImageView.sd_cancelCurrentImageLoad()
+    }
+
 }
